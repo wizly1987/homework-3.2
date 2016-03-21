966a68309b30339c07c70dc640f2c9c509b90415
wizly1987 <vasya.87@mail.ru>
2016-03-21 21:03:12 +0200

    Added files via upload

---
+++ MusicalInstrument.java
@@ -0,0 +1,29 @@
+package com.goit.gojavaonline;
+
+
+
+
+public class MusicalInstrument {
+    public void main (String[]args ) {
+
+        class Piano extends MusicalInstrument {
+            public void play() {
+            }
+        }
+        class Guitar extends MusicalInstrument {
+            public void play() {
+            }
+        }
+        class Trumpet extends MusicalInstrument {
+            public void play() {
+            }
+
+            class MusicalShop {
+                public void open() {
+                }
+            }
+        }
+    }
+    }
+
+
